1883b722d8a647779e1904edee037cfa15e2d209
Nils Smolinski <nils.smolinski@lyth.de>
2021-01-15 09:04:57 +0100

    init

---
+++ src/main/java/de/smotastic/microservices/springcloudresilience/SpringCloudResilienceApplication.java
@@ -0,0 +1,13 @@
+package de.smotastic.microservices.springcloudresilience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudResilienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudResilienceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/smotastic/microservices/springcloudresilience/SpringCloudResilienceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.smotastic.microservices.springcloudresilience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudResilienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
